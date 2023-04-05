7f82a2388009cd20f1991f4768013d10192d0ea5
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2023-04-06 01:56:09 +0900

    5. 버튼, 점수판 만들기

--- SwiftStudentChallenge2023.swiftpm/ContentView.swift
+++ SwiftStudentChallenge2023.swiftpm/ContentView.swift
@@ -1,9 +1,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject var game = Game()
+    
     var body: some View {
         ZStack {
             ARVCRepresentable()
+            VStack {
+                Text("\(game.score)")
+                    .font(.title)
+                    .fontWeight(.bold)
+                
+                Spacer()
+                
+                Button {
+                    game.score += 1
+                } label: {
+                    Circle()
+                        .frame(width: 50, height: 50)
+                        .foregroundColor(.red)
+                }
+
+            }
+            .padding(.all, 100)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .edgesIgnoringSafeArea(.all)
---
+++ SwiftStudentChallenge2023.swiftpm/Game.swift
@@ -0,0 +1,14 @@
+//
+//  Game.swift
+//  SwiftStudentChallenge2023
+//
+//  Created by 강민규 on 2023/04/06.
+//
+
+import Foundation
+
+class Game: ObservableObject {
+    @Published var score: Int = 0
+    
+    
+}
