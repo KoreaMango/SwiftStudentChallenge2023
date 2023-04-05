ca0cec4c337fb7d1915173f281d6770721b4b487
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2023-04-05 20:04:06 +0900

    4. 투호 통 만들기

--- SwiftStudentChallenge2023.swiftpm/ARViewController.swift
+++ SwiftStudentChallenge2023.swiftpm/ARViewController.swift
@@ -5,6 +5,7 @@
 //  Created by 강민규 on 2023/04/04.
 //
 
+import Foundation
 import UIKit
 import SceneKit
 import ARKit
@@ -25,9 +26,16 @@ class ARViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
     //MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        let jar = makeJar()
+        let underJar = makeUnderJar()
         
+        
+        // AddView, Node
         self.view.addSubview(sceneView)
+        sceneView.scene.rootNode.addChildNode(jar)
+        sceneView.scene.rootNode.addChildNode(underJar)
         
+        // Layout
         NSLayoutConstraint.activate([
             sceneView.topAnchor.constraint(equalTo: view.topAnchor),
             sceneView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
@@ -42,12 +50,9 @@ class ARViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
         configuration.planeDetection = .horizontal
         configuration.isLightEstimationEnabled = true
-        
         sceneView.session.run(configuration)
-        
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -55,8 +60,38 @@ class ARViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
     }
     
     //MARK: - Scene Function
+    func makeJar() -> SCNNode {
+        let jarNode = SCNNode()
+
+        let outMaterial = SCNMaterial()
+        let inMaterial = SCNMaterial()
+        
+        let jarOut = SCNTube(innerRadius: 0.08, outerRadius: 0.10, height: 0.25)
+        
+        outMaterial.diffuse.contents = UIImage(named: "TreeTexture")
+        inMaterial.diffuse.contents = UIImage(named: "InnerTreeTexture")
+        
+        jarNode.geometry = jarOut
+        jarNode.geometry?.materials = [outMaterial, inMaterial]
+        jarNode.position = SCNVector3(0, -1, -1)
+        
+        return jarNode
+    }
     
-    
-    
+    func makeUnderJar() -> SCNNode {
+        let underJarNode = SCNNode()
+        
+        let underMaterial = SCNMaterial()
+        
+        let jarUnder = SCNCylinder(radius: 0.08, height: 0.05)
+        
+        underMaterial.diffuse.contents = UIImage(named: "UnderTreeTexture")
+        
+        underJarNode.geometry = jarUnder
+        underJarNode.geometry?.materials = [underMaterial]
+        underJarNode.position = SCNVector3(0, -1.05, -1)
+        
+        return underJarNode
+    }
 }
 
