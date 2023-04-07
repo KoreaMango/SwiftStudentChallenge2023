cc89b51acc5408a4958892cddf2827d5cac2868a
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2023-04-07 22:32:13 +0900

    6. 화면 클릭시 노드 생성

--- SwiftStudentChallenge2023.swiftpm/ARViewController.swift
+++ SwiftStudentChallenge2023.swiftpm/ARViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ARViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
+class ARViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate, UIGestureRecognizerDelegate {
     //MARK: - Properties
+    // AR Scene View
     lazy var sceneView: ARSCNView = {
         let scene = ARSCNView()
         scene.delegate = self
@@ -21,20 +22,42 @@ class ARViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
         return scene
     }()
     
+    // 투호 점수
+    private var score: UInt = 0 {
+           didSet {
+               DispatchQueue.main.async { [unowned self] in
+                   self.scoreLabel.text = "\(self.score)"
+                   
+               }
+           }
+       }
+    
+    // 점수 Label
+    private let scoreLabel = UILabel(frame: CGRect(x: 0,
+                                                   y: UIScreen.main.bounds.height / 8,
+                                                   width: UIScreen.main.bounds.width,
+                                                   height: 100))
+    
+    // ARSession에
     let configuration = ARWorldTrackingConfiguration()
     
     //MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Make Jar Object
         let jar = makeJar()
         let underJar = makeUnderJar()
         
-        
-        // AddView, Node
+        // Tap Recognizer
+        let tapGestureRecognizer = UITapGestureRecognizer()
+        tapGestureRecognizer.delegate = self
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+
         self.view.addSubview(sceneView)
         sceneView.scene.rootNode.addChildNode(jar)
         sceneView.scene.rootNode.addChildNode(underJar)
-        
+       
         // Layout
         NSLayoutConstraint.activate([
             sceneView.topAnchor.constraint(equalTo: view.topAnchor),
@@ -50,9 +73,17 @@ class ARViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        //
         configuration.planeDetection = .horizontal
         configuration.isLightEstimationEnabled = true
         sceneView.session.run(configuration)
+        
+        score = 0
+        scoreLabel.font = UIFont(name: "HelveticaNeue", size: 100.0)
+        scoreLabel.textColor = .white
+        scoreLabel.textAlignment = .center
+        
+        sceneView.addSubview(scoreLabel)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -68,9 +99,15 @@ class ARViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
         
         let jarOut = SCNTube(innerRadius: 0.08, outerRadius: 0.10, height: 0.25)
         
+        let jarPhysicsBody = SCNPhysicsBody(
+            type: .static,
+            shape: SCNPhysicsShape(geometry:  SCNTube(innerRadius: 0.08, outerRadius: 0.10, height: 0.25))
+        )
+        
         outMaterial.diffuse.contents = UIImage(named: "TreeTexture")
         inMaterial.diffuse.contents = UIImage(named: "InnerTreeTexture")
         
+        jarNode.physicsBody = jarPhysicsBody
         jarNode.geometry = jarOut
         jarNode.geometry?.materials = [outMaterial, inMaterial]
         jarNode.position = SCNVector3(0, -1, -1)
@@ -85,13 +122,57 @@ class ARViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
         
         let jarUnder = SCNCylinder(radius: 0.08, height: 0.05)
         
+        let jarPhysicsBody = SCNPhysicsBody(
+            type: .static,
+            shape: SCNPhysicsShape(geometry: SCNCylinder(radius: 0.08, height: 0.05))
+        )
+        
         underMaterial.diffuse.contents = UIImage(named: "UnderTreeTexture")
         
+        underJarNode.physicsBody = jarPhysicsBody
         underJarNode.geometry = jarUnder
         underJarNode.geometry?.materials = [underMaterial]
         underJarNode.position = SCNVector3(0, -1.05, -1)
         
         return underJarNode
     }
+    
+    func makeArrow() -> SCNNode {
+        // 1
+        let arrowNode = SCNNode()
+        
+        let material = SCNMaterial()
+        
+        let arrow = SCNCylinder(radius: 0.01, height: 0.3)
+        
+        let arrowPhysicsBody = SCNPhysicsBody(
+            type: .dynamic,
+            shape: SCNPhysicsShape(geometry: SCNCylinder(radius: 0.01, height: 0.3))
+        )
+        
+        material.diffuse.contents = UIColor.red
+        
+        arrowNode.geometry = arrow
+        arrowNode.geometry?.materials = [material]
+        
+        arrowPhysicsBody.mass = 1
+        arrowPhysicsBody.friction = 2
+        arrowPhysicsBody.contactTestBitMask = 1
+
+        arrowNode.physicsBody = arrowPhysicsBody
+        arrowNode.position = SCNVector3(0, 0, -1)
+      
+        // 2
+        //currentBallNode = ballNode
+        return arrowNode
+    }
+    
+    // MARK: - Tap Action
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        let arrow = makeArrow()
+        sceneView.scene.rootNode.addChildNode(arrow)
+        return true
+    }
+   
 }
 
